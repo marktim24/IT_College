a5ef42267d399208b494c9909f951a2b23682539
Mark <timoshevich199524@mail.ru>
2022-12-15 16:11:52 +0200

    Folder

---
+++ Vladteh/Algorithms/Algorithms/Java/Difficulty_algorithms/diffucultyEstimateExamples/diffuculty_estimate_5.java
@@ -0,0 +1,26 @@
+public class Example5 {
+
+	//time: O(n), memory: O(n)
+
+	private final Map<Integer, Integer> map = new HashMap<>();
+
+	public int getFibonacci (int n) {
+
+		if (n == 0) {
+			return 0;
+		}
+
+		if (n == 1) {
+			return 1;
+		}
+
+		if (map.containsKey(n)) {
+			return map.get(n);
+		}
+
+		int fibonacci = getFibonacci(n-1) + getFibonacci(n-2);
+		map.put(n, fibonacci)
+		return fibonacci;
+	}
+
+	// 5

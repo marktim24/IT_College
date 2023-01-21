572c3437071566f855062a662b459adcf29c006c
Mark <timoshevich199524@mail.ru>
2023-01-21 15:26:17 +0200

    First page Image fix

--- Koopteh/Practice projects/7 Workout React Web App/front/src/components/Layout/Layout.jsx
+++ Koopteh/Practice projects/7 Workout React Web App/front/src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../Header/Header'
 import styles from './Layout.module.scss'
 
-const Layout = ({ children, bgImg, height }) => {
+const Layout = ({ children, bgImg, height = '500px' }) => {
 	return (
 		<div
 			className={styles.wrapper}
